a92866c286ea27c6c8a4adbaac454d6db13538e9
IsabelM <Isabelvmn93@gmail.com>
2023-11-26 10:45:28 +0000

    recuperados elementos compartidos en hijo

--- PECS/Plantilla Trabajo DyASO/DyASO_PED2_Manzaneque_Nunez_Isabel/Trabajo2/hijo.c
+++ PECS/Plantilla Trabajo DyASO/DyASO_PED2_Manzaneque_Nunez_Isabel/Trabajo2/hijo.c
@@ -4,24 +4,50 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-
-char  texto[20];
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
 
 int main(int argc, char const *argv[]) {
-/*   
-    printf("\n------------ Inicio nene ------------\n");
+   
+    printf("\n------------ Inicio hijo ------------\n");
+    
+    // Recuperar la clave
+    key_t key = ftok(argv[1],'X');
 
+    // recuperar cola de mensajes
+    int mensajes = msgget(key,0);
+    if (mensajes == -1) {
+        perror("mensajes");
+        exit(-1);
+    }
+
+    // recuperar semaforo
+    int sem = semget(key, 0, 0);
+    if (sem == -1) {
+        perror("semaforo");
+        exit(-1);
+    }
+
+    // recuperar memoria compartida
+    int shrdMemId = shmget(key, 0);      
+    if (mensajes == -1) {
+        perror("memoriaCompartida");
+        exit(-1);
+    }
+
+    char texto[20];
     strcpy(texto, argv[0]);
     printf("arg = %s\n\n",texto);
+
     for (int i=1;i<=5;i++){
         printf("%s --> %d\n",texto,i);
         sleep(1);
     }
     
-    printf("-------------- Fin nene -------------\n\n");
+    printf("-------------- Fin hijo -------------\n\n");
     exit(0);
-*/
 
-    printf("hola!\n");
-    exit(0);
+
+
 }
--- PECS/Plantilla Trabajo DyASO/DyASO_PED2_Manzaneque_Nunez_Isabel/Trabajo2/padre.c
+++ PECS/Plantilla Trabajo DyASO/DyASO_PED2_Manzaneque_Nunez_Isabel/Trabajo2/padre.c
@@ -12,41 +12,60 @@
 #include <sys/wait.h>
 
 
-int main(int argNum, char *args[]){
+int main(int argc, char *argv[]){
     
-    if(argNum != 2){
+    if(argc != 2){
         printf("Padre se invoca con un argumento!\n");
 	return 0;
     } 
+    int N = atoi(argv[1]);
+
     // crea clave asociada a fichero ejecutable y letra 
-    key_t key = ftok(args[0],'X');
+    key_t key = ftok(argv[0],'X');
 
     // crear cola de mensajes "mensajes"
     int mensajes = msgget(key, IPC_CREAT | 0600);
  
     // crear región de memoria compartida "lista" de tamano 10 pids
     // y enlazararla con un array con capacidad para N PIDs 
-    int arraySize = (int)args[1]*sizeof(pid_t);
+    int arraySize = N*sizeof(pid_t);
     int shrdMemId = shmget(key, arraySize, IPC_CREAT | 0600);      
     pid_t *lista = (pid_t*)shmat(shrdMemId, NULL, 0);
 
     // crear un semáforo para proteger acceso a "lista" 
-    int semaphoreId = semget(key, 1, IPC_CREAT | 0600);
+    int sem = semget(key, 1, IPC_CREAT | 0600);
 
     // crear tuberia sin nombre "barrera"
-    int pipeArray[2];
-    int barrera = pipe(pipeArray);
+    int barrera[2];
+    if(pipe(barrera) == -1){
+        perror("pipe");
+        exit(-1);
+    }
     
     // crea N procesos hijos que ejecutaran HIJO
-    for(int i = 0; i < 10; i++){
+    
+    for(int i = 0; i < N; i++){
 
-        pid_t pidFork = fork();
+        pid_t resFork = fork();
         
-        if(pidFork == 0){	   	    
-            
-            execl("./Trabajo2/HIJO", "HIJO", NULL); 
-	    printf("\n Aqui ya no llego\n");          
-	
+	if(resFork == -1){
+            perror("fork");
+            exit(-1);
+        }
+
+        if(resFork == 0){	   	    
+            //Esto lo ejecuta solo el hijo
+
+            // Cierra la salida estándar y duplicar extremo de lectura de barrera
+            // tambien: close(1); dup(barrera[0]);
+            //close(1); 
+            dup(barrera[0]);
+            close(barrera[0]);
+            close(barrera[1]);
+
+	    printf("Ejecutando hijo %d con PID %d\n", i, getpid()); 
+            execl("./Trabajo2/HIJO", "HIJO",argv[0], NULL); 
+
         }        
     }
     
@@ -55,7 +74,7 @@ int main(int argNum, char *args[]){
     //shmdt();
 
     wait(0);
-    printf("Finalizando padre \n");       
+    printf("Finalizando padre\n");       
     return 0;
 
 }
